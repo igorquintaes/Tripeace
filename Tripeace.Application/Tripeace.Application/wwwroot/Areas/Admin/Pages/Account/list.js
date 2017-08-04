5f6d4b81ef6af2421f27d975efa24f54c1686097
Igor Quintaes <igor.quintaes@fatec.sp.gov.br>
2017-08-04 19:24:15 -0300

    button toggle ban/unban (I HATE FRONT-END)

--- Tripeace.Application/Tripeace.Application/wwwroot/Areas/Admin/Pages/Account/list.js
+++ Tripeace.Application/Tripeace.Application/wwwroot/Areas/Admin/Pages/Account/list.js
@@ -1,16 +1,20 @@
 ﻿$(document).ready(function ($) {
-    $(".btn-ban").click(function () {
-        var idToBan = $(this).attr("data-id");
-        $("#modal-ban-confirmation").attr("id-to-ban", idToBan);
+    $(".btn-ban-action").click(function () {
+        var accountId = $(this).attr("data-id");
+        var action = $(this).attr("data-action");
 
-        $("#ban-reason").val("");
+        if (action == "ban") {
+            $("#modal-ban-confirmation").attr("id-to-ban", accountId);
+            $("#ban-reason").val("");
+        }
+
+        if (action == "unban") {
+            $("#modal-unban-confirmation").attr("id-to-unban", accountId);
+            $("#unban-reason").val("");
+        }
     }),
 
     $(".btn-unban").click(function () {
-        var idToUnban = $(this).attr("data-id");
-        $("#modal-unban-confirmation").attr("id-to-unban", idToUnban);
-
-        $("#unban-reason").val("");
     }),
 
     $(function () {
@@ -18,34 +22,35 @@
             startDate: '+1d',
             autoclose: true
         });
-        }),
+    }),
 
-        $("#ban-submit").click(function () {
-            var dataType = 'application/json; charset=utf-8';
-            var model = {
-                "Id": parseInt($("#modal-ban-confirmation").attr("id-to-ban")),
-                "Reason": $("#ban-reason").val(),
-                "Date": $("#datetimepicker-ban").val()
-            };
+    $("#ban-submit").click(function () {
+        var dataType = 'application/json; charset=utf-8';
+        var model = {
+            "Id": parseInt($("#modal-ban-confirmation").attr("id-to-ban")),
+            "Reason": $("#ban-reason").val(),
+            "Date": $("#datetimepicker-ban").val()
+        };
 
-            var json = JSON.stringify(model);
+        var json = JSON.stringify(model);
 
-            $('#modal-ban-confirmation').modal('hide');
+        $('#modal-ban-confirmation').modal('hide');
 
-            $.ajax({
-                type: 'POST',
-                url: '/Admin/Account/Ban',
-                dataType: 'json',
-                contentType: 'application/json',
-                data: json,
-                success: function (result) {
-                    $("#modal-server-response-title").html(result.title);
-                    $("#modal-server-response-body").html(result.message);
+        $.ajax({
+            type: 'POST',
+            url: '/Admin/Account/Ban',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: json,
+            success: function (result) {
+                $("#modal-server-response-title").html(result.title);
+                $("#modal-server-response-body").html(result.message);
+                $("#modal-server-response").modal("show");
 
-                    $("#modal-server-response").modal("show");
-                }
-            });
+                toogleButton("unban", model.Id);
+            }
         });
+    });
 
     $("#unban-submit").click(function () {
         var dataType = 'application/json; charset=utf-8';
@@ -67,9 +72,26 @@
             success: function (result) {
                 $("#modal-server-response-title").html(result.title);
                 $("#modal-server-response-body").html(result.message);
-
                 $("#modal-server-response").modal("show");
+
+                toogleButton("ban", model.Id);
             }
         });
     });
+
+    function toogleButton(newButton, id) {
+        var button = $(".btn-ban-action[data-id='" + id + "']");
+
+        if (newButton == "ban") {
+            button.html('<i class="fa fa-warning"></i> &thinsp;&thinsp;' + localizer.BanButton);
+            button.attr("data-target", "#modal-ban-confirmation");
+            button.attr("data-action", "ban");
+        }
+
+        if (newButton == "unban") {
+            button.html('<i class="fa fa-warning"></i> &thinsp;&thinsp;' + localizer.UnbanButton);
+            button.attr("data-target", "#modal-unban-confirmation");
+            button.attr("data-action", "unban");
+        }
+    }
 });
